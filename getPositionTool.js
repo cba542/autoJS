53ea6426995c810a927d9b854f1eaa73a531943b
Sam Cheng <cba542@hotmail.com>
2025-02-05 21:43:34 +0800

    feat: Add coordinate capture functionality using AutoJS transparent overlay

---
+++ getPositionTool.js
@@ -0,0 +1,12 @@
+var u = floaty.rawWindow(
+    <frame id="view" bg="#00000000" />
+);
+u.setSize(-1, -1);
+setInterval(() => { }, 1000);
+u.view.setOnTouchListener(function (v, t) {
+    if (t.getAction() == t.ACTION_DOWN) {
+        toastLog("当前点击的坐标为：" + t.getRawX() + "," + t.getRawY());
+        return true;
+    }
+    return true;
+});
